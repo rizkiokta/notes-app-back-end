6091d8a894e10f3b856d4ba88e534075e508a881
Muhammad Rizki Oktafianto <mrizkioktafianto@gmail.com>
2023-01-13 20:13:22 +0700

    bugs oon ENV

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-  console.log(process.env.NODE_ENV);
   const server = Hapi.server({
     port: 5001,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
